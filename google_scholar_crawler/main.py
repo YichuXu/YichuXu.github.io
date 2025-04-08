5f456ee007f34fb0e58bbd77f8a166596da9b5c3
LYL1015 <2037269790@qq.com>
2025-04-08 10:18:18 -0400

    update wechat

--- google_scholar_crawler/main.py
+++ google_scholar_crawler/main.py
@@ -4,7 +4,7 @@ import json
 from datetime import datetime
 import os
 
-# 直接设置ID
+# Set Google Scholar ID directly
 os.environ['GOOGLE_SCHOLAR_ID'] = "5F3tICwAAAAJ"
 author: dict = scholarly.search_author_id(os.environ['GOOGLE_SCHOLAR_ID'])
 scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
